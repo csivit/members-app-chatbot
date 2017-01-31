41b7f0920562d4efcf6c40bc3be034d209fec5c0
Pradyun S. Gedam <pradyunsg@gmail.com>
2017-02-01 03:03:21 +0530

    Add a mock implementation
    This is intended to give something for the other team to work with, allowing them to give feedback or utilize this.

---
+++ README.md
@@ -0,0 +1,4 @@
+# ChatBot
+A chatbot developed for CSI-VIT's member's application. Currently, under active development.
+
+The intention is that the ChatBot serves as an assistant for the users, allowing them to ask questions (possibly in a conversational manner) and get answers.
---
+++ chatbot.py
@@ -0,0 +1,58 @@
+"""A chatbot developed for CSI-VIT's member's application.
+
+The following is a simple usage example::
+
+  import chatbot
+
+  # Initialize for a new chat
+  bot = chatbot.ChatBot(context=None)
+
+  # Get a response from the bot
+  print(bot.get_response("Hello!"))
+
+  # Get a JSON dump of the current state of the bot
+  dump = bot.serialize()
+
+  # Recreate a bot by providing the same JSON dump as context.
+  bot2 = chatbot.ChatBot(context=dump)
+
+  # Get a response from the bot
+  print(bot.get_response("When is the next CSI event?"))
+"""
+
+# TODO: Add tests.
+
+
+class ChatBot(object):
+    """Represents a chatbot for a certain conversation.
+
+    .. note::
+        We might have to pass the events list as a separate parameter.
+
+    :param context: A previously serialized version of a bot. Do not pass if there is no such previous bot
+    """
+
+    def __init__(self, context: str = "") -> None:
+        super(ChatBot, self).__init__()
+        self._context = self._deserialize(context)
+
+    # TODO: Actually do something.
+    def serialize(self):
+        """Returns a JSON object that represents the state of the context.
+        """
+        return "{}"
+
+    def get_response(self, message: str) -> str:
+        """Get a response for the user's message.
+
+        :param message: The message from user to generate a response for.
+        :rtype: A string response
+        """
+        return "You said - " + message
+
+    @staticmethod
+    def _deserialize(dump: str) -> 'Dict[str, Any]':
+        """Returns a JSON object that represents the state of the context
+        object.
+        """
+        return {}
